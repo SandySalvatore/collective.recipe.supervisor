b98099c149647ddbed998926649434d6c4166d1d
SandySalvatore <ts-lankishetty.sande@rakuten.com>
2019-08-07 12:27:48 +0530

    Version modified

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.20.post2'
+version = '0.20.post2.altair'
 
 long_description = (
     read('README.rst') + '\n\n' +
